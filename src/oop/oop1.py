3929dcb8599948d0d49443d728b39c39a34e7460
toneiojimz <ajimeneza77@gmail.com>
2020-08-07 08:40:16 -0700

    finished oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,26 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: #base class for all vehicles
+    pass
+
+class FlightVehicle(Vehicle): #base class for flight vehilces and subclass of vehicles
+    pass
+
+class Starship(FlightVehicle, Vehicle): #base class for startships ans subclass of flightVehicle
+    pass
+
+class Airplane(FlightVehicle, Vehicle): # subclass of flightvehicle and vehicles
+    pass
+
+
+class GroundVehicle(Vehicle): #subclass of vehicle and base class for ground vehicles
+    pass
+
+class Car(GroundVehicle, Vehicle): #subclass of groundvehicle and inherits from vehicle
+    pass
+
+class Motorcycle(GroundVehicle): #subclass of groundvehicle and inherits from vehicle 
+    pass
+
